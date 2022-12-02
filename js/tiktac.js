31deeb171b9f13057a893d24279751cf246f7d0f
Nikita Gromov <nikita.gromoff@gmail.com>
2022-12-02 13:28:59 +0300

    initiate upload

--- js/tiktac.js
+++ js/tiktac.js
@@ -1,35 +1,88 @@
 'use strict'
+
+/* Variables */
+
+let saveGameButton = document.querySelector('#save--button');
 let check = document.querySelector('#checkbox');
-let switcher = document.querySelector('#switcher');
+let switchText = document.querySelector('#switchText');
 let rows = document.querySelectorAll('.active');
-let resetBtn = document.querySelector('#reset--button');
+let newGameButton = document.querySelector('#new--game--button');
 let resetScore = document.querySelector('#reset--score');
 let result = document.querySelector('#winner');
 let p1 = document.querySelector('#p1');
 let p2 = document.querySelector('#p2');
 
-console.log(rows);
-localStorage.setItem('data',JSON.stringify(rows))
-console.log(JSON.parse(localStorage.getItem('data')));
+let winCombs = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[8,7,6]];
+let [playerX, playerO, counter, winner,score] = [[],[],0,false,[0,0]];
 
 
-let [playerO, playerX, counter, winner,score] = [[],[],0,false,[0,0]];
+/* check for saved games in localStorage and display saved games if it is*/
 
-check.addEventListener('change', checkPriority)
+if (localStorage.getItem('data')) {
+  let data = localStorage.getItem('data');
+  data = JSON.parse(data);
+  document.querySelector('.saved--game').innerHTML = `Saved game ${data['date']} <div><a onclick="loadGame()">Load saved</a></div> <div><a onclick="removeSaved()">Remove saved</a></div>`;
+}
+
+/* Event with callback func that stringify JSON on push variables in localStorage */
+
+saveGameButton.addEventListener('click', () => {
+
+  let data = {};
+  for (let el of rows){
+    data[el.value] = el.textContent;
+  }
+  data['moves'] = [playerX, playerO];
+  let date = new Date;
+  data['date'] = date.toLocaleString();
+  data['score'] = score;
+  console.log(data);
+  localStorage.setItem('data',JSON.stringify(data))
+  document.querySelector('.saved--game').innerHTML = `Saved game ${data['date']} <div><a onclick="loadGame()">Load saved</a></div> <div><a onclick="removeSaved()">Remove saved</a></div>`;
+})
+
+/* Func that load already saved object 'data' from localStorage and pull variables */
+
+function loadGame() {
+  let data = localStorage.getItem('data');
+  data = JSON.parse(data);
+  for (let el of rows){
+    el.textContent = data[`${el.value}`]
+    el.setAttribute('ready', `${el.textContent != '' ? 'false' : 'true'}`);
+  }
+  playerX = data['moves'][0];
+  playerO = data['moves'][1];
+  score = data['score'];
+  p1.innerHTML = `P1: ${score[0]}`
+  p2.innerHTML = `P2: ${score[1]}`
+
+}
+
+/* Func that erase any saved data called 'data' on localStorage*/
+
+function removeSaved() {
+  localStorage.removeItem('data');
+  document.querySelector('.saved--game').innerHTML = ``;
+}
+
+/* Check Priority for first move X or O */
+
 function checkPriority() {
   if(check.checked) {
     reset();
-    switcher.innerHTML = 'P1 move first';
+    switchText.innerHTML = 'P1 move first';
     counter = 0;
   }
   else {
     reset();
-    switcher.innerHTML = 'P2 move first';
+    switchText.innerHTML = 'P2 move first';
     counter = 1;
   }
 }
 
-let winCombs = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[8,7,6]];
+check.addEventListener('change', checkPriority)
+
+/* Compare function check for player Combination include any winning combination */
 
 function compare(winCombs, playerComb){
             for( let i = 0 ; i < winCombs.length ; i++) {
@@ -42,36 +95,47 @@ function compare(winCombs, playerComb){
             }
             return false;
           }
+
+ /* Call for function moveFunction() when click on any cell */
+
     for (let el of rows) {
         el.addEventListener('click', moveFunction);
-        
     }
 
+/* Function of gameplay */
+
     function moveFunction() {
+
+                  /* Check for draw */ 
 
         if((playerO.length + playerX.length) > 7 && !winner){
           result.innerHTML = 'It\'s a draw!';
         }
+
+/* Check X for possibilaty make a move and then check for win */
+
           if (!(counter % 2) && !winner && this.getAttribute('ready') == 'true') {
           this.setAttribute('ready', 'false');
           this.innerHTML = 'X'
           playerX.push(this.value)
           counter++;
-          console.log( this.status != 'checked');
-             if(playerX.length > 2 && compare(winCombs, playerX)) {
+             if (playerX.length > 2 && compare(winCombs, playerX)) {
               result.innerHTML = 'Player 1 win!';
               score[0]++
               p1.innerHTML = `P1: ${score[0]}`
               winner = true;
             }
         }  
+
+/* Check O for possibilaty make a move and then check for win */
+
           else if ((counter % 2) && !winner && this.getAttribute('ready') == 'true') {
             this.setAttribute('ready', 'false');
             this.innerHTML = 'O'
           playerO.push(this.value)
           counter++;
-          console.log(this.status);
-             if(playerO.length > 2 && compare(winCombs, playerO)) {
+
+             if (playerO.length > 2 && compare(winCombs, playerO)) {
                 result.innerHTML = 'Player 2 win!';
               winner = true;
               score[1]++
@@ -81,7 +145,9 @@ function compare(winCombs, playerComb){
          }
         }
 
-  resetBtn.addEventListener('click', reset)
+  /* Event on new game button erase players arrays, set winner = false, check for who's first to move, erase content from cells */
+        
+  newGameButton.addEventListener('click', reset)
 
   function reset(){
     [playerO, playerX, winner] = [[],[],false];
@@ -92,16 +158,24 @@ function compare(winCombs, playerComb){
                   el.setAttribute('ready', 'true');
     }
 }
+
+/* Event on reset score button with callback func that erase score */
+
   resetScore.addEventListener('click', () => {
     score = [0,0];
     p1.innerHTML = `P1: ${score[0]}`
     p2.innerHTML = `P2: ${score[1]}`
   })
+
+ /* Function that generate gradient colors and set it on game field background */
+
 function getRandomGradient() {
-
 let getRgb = () => Math.floor(Math.random() * 255);
-
 let bg = `linear-gradient(45deg, rgb(${getRgb()} ${getRgb()} ${getRgb()}), rgb(${getRgb()} ${getRgb()} ${getRgb()}))`;
-document.querySelector('body').style.background = bg;
+// document.querySelector('body').style.background = bg;document.querySelector('body').style.background = bg;
+document.querySelector('.container').style.background = bg;
 }
+
+/* Call for func */
+
 getRandomGradient()
